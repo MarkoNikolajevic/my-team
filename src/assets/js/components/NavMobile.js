3dfa68b9c0a1b2da8a6b65de97a8ecb41d7ecbf6
Marko Nikolajevic <mrknikolajevic@gmail.com>
2020-03-13 19:03:28 +0100

    Add links to mobile nav

--- src/assets/js/components/NavMobile.js
+++ src/assets/js/components/NavMobile.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as BgPattern } from '../../img/bg-pattern-about-1-mobile-nav-1.svg';
+
 const NavMobile = () => {
   return (
     <nav>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <Link to='/'>home</Link>
+        </li>
+        <li>
+          <Link to='/about'>about</Link>
+        </li>
+        <li>
+          <Link to='/contact'>contact us</Link>
+        </li>
+        <BgPattern />
       </ul>
     </nav>
   );
